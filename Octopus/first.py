13da2a1ab524532c69695ed979049f4a07d80d15
YASH000911 <133102014+YASH000911@users.noreply.github.com>
2024-10-11 00:37:20 +0530

    Add files via upload

---
+++ Octopus/first.py
@@ -0,0 +1,13 @@
+prin="hello hii"
+slicing = prin[1:4:2]
+print(slicing)
+
+print(prin.startswith("hel"))
+print(prin.endswith("i"))
+print(prin.capitalize())
+print(prin.upper())
+print(prin.lower())
+print(len(prin))
+b = "sushmitha "
+print(len(b))
+print(b[0:6:2])
